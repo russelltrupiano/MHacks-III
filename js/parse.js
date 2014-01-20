c77d2155b7c3c343662ed86c1ee8b4fe761f5cd9
russelltrupiano <russelltrupiano@gmail.com>
2014-01-20 00:38:41 -0500

    need to create methods to populate data

--- js/parse.js
+++ js/parse.js
@@ -1,4 +1,6 @@
 Parse.initialize("SvtOXpjNcHr97ff1Ii1Z3v9t9h8glThKmUByUCji", "shMVnpVWSjl12KcbONuCobrhYCmSWujzs7QMhAlW");
+var Recipe = Parse.Object.extend("Recipe");
+
 
 // Here 'null' is replaced witht he permissions the app needs
 // ex) Parse.FacebookUtils.logIn("user_likes, email", {//body});
@@ -118,7 +120,7 @@ function assemble_actions() {
 }
 
 function pushRecipe() {
-  var Recipe = Parse.Object.extend("Recipe");
+  // var Recipe = Parse.Object.extend("Recipe");
   var recipe = new Recipe();
 
   var name_p = $("#recipe-name").val();
@@ -147,7 +149,6 @@ function pushRecipe() {
 }
 
 function fetchUserData() {
-  var Recipe = Parse.Object.extend("Recipe");
   var query = new Parse.Query(Recipe);
   FB.api(
     "/me", function(response){
@@ -157,8 +158,8 @@ function fetchUserData() {
           console.log(results.length);
           console.log(results);
           for (var i = 0; i < results.length; i++) { 
-            var object = results[i];
-            console.log(object.id + ' - ' + object.get('name'));
+            var obj = results[i];
+            console.log(obj.id + ' - ' + obj.get('name') + ":  " + obj.get("time"));
           }
         },
         error: function(error) {
@@ -168,3 +169,48 @@ function fetchUserData() {
     }
   );
 }
+
+// function populateRecipeView(parent) {
+  
+//   var query = Parse.Query("Recipe");
+//   query = fetchUserData();
+
+//   console.log(query);
+
+//   for (var i = 0; i < query.length; i++) { 
+//     var obj = query[i];
+
+//     var recipe_view = "<a href='#' id='recipe-click-view-"+i+"'>
+//         <div class='recipe-view container' id='recipe-"+i+"'>
+//           <div class='recipe-view-text'>
+//             <p class='recipe-view-title'><a id='recipe-"+i+"' href='#'><span class='lead'>"+obj.get("name")+"</span></a><span class='recipe-view-time'>         ("+obj.get("time")+")</span></p>
+//             <div class='social-share'>
+//               <div class='pinterest-share-button share-button'>
+//                 <a href='http://www.pinterest.com/pin/create/button/
+//                     ?url=http%3A%2F%2Fwww.flickr.com%2Fphotos%2Fkentbrew%2F6851755809%2F
+//                     &media=http%3A%2F%2Ffarm8.staticflickr.com%2F7027%2F6851755809_df5b2051c9_z.jpg
+//                     &description=Next%20stop%3A%20Pinterest'
+//                     data-pin-do='buttonPin'
+//                     data-pin-config='above'>
+//                     <img src='//assets.pinterest.com/images/pidgets/pin_it_button.png' />
+//                 </a>
+//               </div>
+//               <div class='fb-share-button share-button' data-href='http://developers.facebook.com/docs/plugins/' data-type='button_count'></div>
+//               <script>(function(d, s, id) {
+//                 var js, fjs = d.getElementsByTagName(s)[0];
+//                 if (d.getElementById(id)) return;
+//                 js = d.createElement(s); js.id = id;
+//                 js.src = 'http://connect.facebook.net/en_US/all.js#xfbml=1&appId=397989610346916';
+//                 fjs.parentNode.insertBefore(js, fjs);
+//               }(document, 'script', 'facebook-jssdk'));</script>
+//             </div>
+//           </div>
+//           <div class='recipe-view-pic'>
+//             <img src='assets/mhacks.png'>
+//           </div>
+//         </div>
+//       </a>";
+//   }
+
+  
+// }
